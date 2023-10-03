41697e87f48fd82bffd6c0cd239d68077f2069bf
Daryl Tan <141814177+daryltwx@users.noreply.github.com>
2023-10-03 11:02:52 +0000

    check_resources() work now

--- coffee/coffee_note.py
+++ coffee/coffee_note.py
@@ -1,9 +1,7 @@
 from menu import MENU, resources
 
 money = 0.00
-water = resources["water"]
-milk = resources["milk"]
-coffee = resources["coffee"]
+
 
 
 # Step 1. Get user's choice
@@ -12,26 +10,32 @@ def main():
         choice = input("What would you like? (espresso/latte/cappuccino): ").lower().strip()
         # Step 1.1 Support for report
         if choice == "report":
-            get_report(money, water, milk, coffee)
+            get_report(money)
 
         # Step 1.2 Support for off
         if choice == "off":
             exit()
 
-        print(check_resources(choice))
+        if check_resources(choice):
+            print("We can make your drink!")
+        else:
+            print("Sorry dude")
+        # print(check_resources(choice))
     
-def get_report(money, water, milk, coffee):
+def get_report(money):
     """
     :takes All the current variables.
     :return str
     """
+    water = resources["water"]
+    milk = resources["milk"]
+    coffee = resources["coffee"]
+
     print(f"Water: {water}ml \nMilk: {milk}ml \nCoffee: {coffee}g \nMoney: ${money}")
 
 
-
-
 # Step 2. Check if there is enough resources to make user's choice
-def check_resources(choice, water, milk, coffee):
+def check_resources(choice):
     """
     :takes user choice
     :returns bool
@@ -40,10 +44,10 @@ def check_resources(choice, water, milk, coffee):
     for i in ingredients:
         if resources[i] - ingredients[i] <= 0:
             return False
+        resources[i] -= ingredients[i]
     
     return True
         
-
 
 def get_ingredients(choice):
     """
@@ -53,6 +57,8 @@ def get_ingredients(choice):
     return MENU[choice]["ingredients"]
 
 # Step 3. If resources is sufficient, start collecting the cost
+
+
 
 # Step 4. Check if the amount was sufficient, generate change if excess.
 # if not, refund. Add the price of coffee to money variable for the report. 
