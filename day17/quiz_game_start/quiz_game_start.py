85afa573f5d6f0b8e0068fd2da79980534ba574e
Daryl Tan <141814177+daryltwx@users.noreply.github.com>
2023-10-05 03:18:04 +0000

    Don't quite understand how to code oop

--- day17/quiz_game_start/quiz_game_start.py
+++ day17/quiz_game_start/quiz_game_start.py
@@ -1,12 +1,13 @@
 from question_model import Question
-from data import question_data
+#from data import question_data
+from api_text import question_data
 from quiz_brain import QuizBrain
 
 
 question_bank = []
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
