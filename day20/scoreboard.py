dda01908973962265be74290f061a599f2d00b7a
Daryl MBP <aboutdaryl@gmail.com>
2023-10-10 18:55:26 +0800

    File read and write

--- day20/scoreboard.py
+++ day20/scoreboard.py
@@ -13,20 +13,33 @@ class Scoreboard(Turtle):
         self.goto(x=0, y=280)
         self.pendown()
         self.score = 0
+        with open("data.txt") as file:
+            data_high_score = int(file.read())
+        self.high_score = data_high_score
         self.update_scoreboard()
 
 
 
+
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
         self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write(f"GAME OVER", move=False, align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.score))
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write(f"GAME OVER", move=False, align=ALIGNMENT, font=FONT)
 
 
