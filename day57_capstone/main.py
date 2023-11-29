a02f023fabac40413860902cb58137fc617b04a4
Daryl MBP <aboutdaryl@gmail.com>
2023-11-29 10:52:47 +0800

    day57 - Completed capstone

--- day57_capstone/main.py
+++ day57_capstone/main.py
@@ -3,12 +3,23 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def blog():
+@app.route('/blog')
+def home():
     blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
     blog_response = requests.get(url=blog_url)
     all_post = blog_response.json()
     return render_template("index.html", posts=all_post)
+
+
+@app.route('/blog/<int:num>')
+def get_blog(num):
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    blog_response = requests.get(url=blog_url)
+    all_post = blog_response.json()
+    return render_template("post.html", posts=all_post, num=num)
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
